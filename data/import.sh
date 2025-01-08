c5e6f2fced705ae62262c2a82fba6febe4690edf
Mark Terpstra <mterpstra@magicleap.com>
2025-01-08 17:23:17 -0500

    Updated for January Events

--- data/import.sh
+++ data/import.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "https://widgets.blackpulp.com/api/v1/gofamilychurch/events?Congregation_ID=16&order_by=Event_Title&Event_Start_Date%3E=2024-12-01&Event_End_Date%3C=2025-01-01" | jq .  > imported_events.json
+curl "https://widgets.blackpulp.com/api/v1/gofamilychurch/events?Congregation_ID=16&order_by=Event_Title&Event_Start_Date%3E=2025-01-01&Event_End_Date%3C=2025-01-31" | jq .  > imported-events.json
